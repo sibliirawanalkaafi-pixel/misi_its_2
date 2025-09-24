5e473d58efb8d2e31e3655a89256630686bc4d6d
sibliirawanalkaafi-pixel <sibliirawanalkaafi@gmail.com>
2025-09-24 08:33:23 +0700

    Add files via upload

---
+++ FormKategori.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PertemuanHariKe_2
+{
+    public partial class FormKategori : Form
+    {
+        public FormKategori()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            cmbKategori.Items.Clear();
+            using (SqlConnection conn = Koneksi.GetConnection())
+            {
+                try
+                {
+                    conn.Open();
+                    string query = "SELECT NamaKategori FROM Kategori";
+                    SqlCommand cmd = new SqlCommand(query, conn);
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        cmbKategori.Items.Add(reader["NamaKategori"].ToString());
+                    }
+                    reader.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Terjadi kesalahan: " + ex.Message);
+                }
+            }
+        }
+
+        private void cmbKategori_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (cmbKategori.SelectedItem != null)
+            {
+                MessageBox.Show("Anda memilih kategori: " + cmbKategori.SelectedItem.ToString());
+            }
+        }
+    }
+}
---
+++ Koneksi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PertemuanHariKe_2
+{
+    internal class Koneksi
+    {
+        public static SqlConnection GetConnection()
+        {
+        string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=TokoDB;Integrated Security=True;";
+            return new SqlConnection(connectionString);
+        }
+    }
+}
